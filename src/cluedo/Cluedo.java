f700d98f859127949b19451eba097bfe1ac81478
Ben Anderson <benanderson@acidic.co.nz>
2012-08-09 13:23:23 +1200

    Cluedo class
    Sorry, I forgot to hit sync

---
+++ src/cluedo/Cluedo.java
@@ -0,0 +1,25 @@
+package cluedo;
+
+public class Cluedo {
+	
+	private final int MaxPlayers = 8;
+	private int nPlayers = 0;
+	
+	private CluedoUI uiVendor;
+	
+	public Cluedo(int numPlayers)
+	{
+		this.nPlayers = numPlayers;
+	}
+	
+	public void Start() throws CluedoException
+	{
+		if(this.nPlayers > this.MaxPlayers)
+			throw new CluedoException(String.format("The number of players must be less than or equal to %d\n", this.MaxPlayers));
+		
+		if(this.uiVendor == null)
+			this.uiVendor = new TextCluedoUI();
+		
+		this.uiVendor.DisplayMessage(CluedoMessage.WelcomeMessage, null);
+	}
+}
