e78cc141ee6ca68fa64917821ba3e27c89cd3cf8
Patrick Barnes <patrick478@gmail.com>
2012-08-12 19:57:37 +1200

    Revert "Add"
    This reverts commit 578966bbda23a9584588e8d3d9ee731ea8c07374.

--- src/cluedo/Character.java
+++ src/cluedo/Character.java
@@ -6,38 +6,30 @@ public class Character extends GameObject {
 	public Character(String n) {
 		this.SetName(n);
 	}
-
+	
 	public static Character SelectCharacter() {
 		int choice = -1;
 		Character retVal = null;
 		Scanner input = new Scanner(System.in);
+		
 		//TODO: Make it so two people cannot select the same character
 		while(choice < 0)
 		{
-			System.out.printf("Select a character:\n");
+			System.out.printf("Select a character:");
 			for(int i = 0; i < characters.length; i++)
-				if(characterChosen[i]==false)
-					System.out.printf("[%d]\t%s\n", i+1, characters[i].GetName());
-			while(input.hasNextInt())
-				choice = input.nextInt()-1;
-			if(choice < characters.length){
-				if(characterChosen[choice]==false){
-					characterChosen[choice] = true;
-					retVal = characters[choice];
-				}
-				else{ System.out.println("Character already chosen");
-				choice = -1;
-				}
-
-			}
+				System.out.printf("[%d]\t%s\n", i+1, characters[i]);
+			
+			choice = input.nextInt();
+			if(choice < characters.length)
+				retVal = characters[choice];
 			else
 				choice = -1;
 		}
+		
+		input.close();
 		return retVal;
 	}
-
-	public static Boolean[] characterChosen = {false, false, false, false, false, false};
-
+	
 	public static final Character[] characters = new Character[] {
 		new Character("Kasandra Scarlet"), 
 		new Character("Jack Mustard"),
