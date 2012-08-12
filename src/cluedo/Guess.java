8dd0f055e99b2dfc3bc067c8cc8f1acedac875ef
Patrick Barnes <patrick478@gmail.com>
2012-08-12 13:50:20 +1200

    Added all character and weapon names
    Also changed field in guess from killer to murderer

--- src/cluedo/Guess.java
+++ src/cluedo/Guess.java
@@ -2,9 +2,13 @@ package cluedo;
 
 public class Guess {
 	public Room room;
-	public Character killer;
+	public Character murderer;
 	public Weapon wep;
 	
+
+	
+
+
 	public static Guess GenerateRandomGuess()
 	{
 		return new Guess();
