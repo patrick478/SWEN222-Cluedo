968ad826177539ce3f32759480eadcbdbc39cb0a
Ben Anderson <benanderson@acidic.co.nz>
2012-09-03 13:19:42 -0700

    added the start of the mouse listener

--- src/cluedo/GUI.java
+++ src/cluedo/GUI.java
@@ -1,6 +1,8 @@
 package cluedo;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,7 +10,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 
-public class GUI extends JFrame implements CluedoUI {
+public class GUI extends JFrame implements CluedoUI, MouseListener {
 
 
 
@@ -252,5 +254,35 @@ public class GUI extends JFrame implements CluedoUI {
 	public void NotifyMoved(int nSteps, Room r) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}	
 }
