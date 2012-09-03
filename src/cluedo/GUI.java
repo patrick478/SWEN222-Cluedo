8bb8e4d56a780c09fa0deb751db9bb62d1c329ab
Ben Anderson <benanderson@acidic.co.nz>
2012-09-03 12:51:25 -0700

    Added board drawing

--- src/cluedo/GUI.java
+++ src/cluedo/GUI.java
@@ -88,7 +88,7 @@ public class GUI extends JFrame implements CluedoUI {
 
 		frame.setJMenuBar(jMenuBar1);
 		
-		frame.setPreferredSize(new Dimension(600,750));
+		frame.setPreferredSize(new Dimension(616,750));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		controls = new JPanel();
--- src/cluedo/GUICanvas.java
+++ src/cluedo/GUICanvas.java
@@ -1,10 +1,34 @@
 package cluedo;
 import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class GUICanvas extends Canvas{
+	Image boardImage;
+	
+	public GUICanvas()
+	{
+		this.boardImage = makeImage("src/resources/board.jpg");
+	}
+	
 	public void paint(Graphics g)
 	{
-		g.setColor(Color.BLUE);
-		g.fillRect(0, 0, 600, 600);
+		g.drawImage(this.boardImage, 0, 0, null);
 	}
+	
+	private BufferedImage makeImage(String filename)
+	{		
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File(filename));
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return img;
+	}	
 }
